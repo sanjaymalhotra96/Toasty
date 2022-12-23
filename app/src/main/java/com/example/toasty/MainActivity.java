cfac7267e71d2043fc319e729306b67c44bd73a0
Dfavo <sanjaymalhotra.dfavo@gmail.com>
2022-12-23 09:59:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/toasty/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.toasty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toaster.simpleToast(this,"Subscriber to papaya toaster");
+    }
+}
